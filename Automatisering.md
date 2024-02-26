a5f87ef593b91748f1ef718f71d7c5b0145fd948
Floris <Floris.vandergraaf@student.hu.nl>
2024-02-26 15:48:13 +0100

    Test Floris

---
+++ Automatisering.md
@@ -0,0 +1,26 @@
+::: mermaid
+graph TB;
+
+    A[<span style="font-size: 20px;">Start<br/><span style="font-size: 15px;">Second Line</span>]
+
+    A --->|Yes| B 
+
+
+    B[<span style="font-size: 20px;">Starting_rechts<br/><span style="font-size: 15px;">Second Line</span>] 
+ 
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
---
+++ Project MSVU/src/main.cpp
@@ -0,0 +1,54 @@
+#include <Arduino.h>
+
+// put function declarations here:
+const int xpin = A0;
+const int Sharp = A1;
+int DriveDir_Forward = 47;
+const int Deadband = 15;
+int xPosition;
+int Amp;
+int xSpeed;
+int test;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(250000);
+  pinMode(DriveDir_Forward, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN,HIGH);
+  xPosition = analogRead(xpin);
+if (xPosition >=(500 - Deadband) && xPosition <=(500 + Deadband)) { //tussen 485+515
+  Serial.println("Direction = Stopped\n");
+  Serial.println(xPosition);
+  analogWrite(2,0);
+  delay(2000);
+  }
+else if (xPosition >500 + Deadband) {  //Groter dan 515
+  Serial.println("Direction = Backwards\n");
+  Serial.println(xPosition); 
+  int mappedValue = map(xPosition, 500, 0, 255, 0);
+ 
+  Serial.println("Mapped Value:");
+  Serial.println(mappedValue);
+  analogWrite(2,mappedValue);
+  //delay(100);
+  }
+else if (xPosition < 500 - Deadband) {
+  Serial.println("Direction = Backwards\n");
+  Serial.println(xPosition); 
+  int mappedValue = map(xPosition, 500, 0, 0, 255);
+ 
+  Serial.println("Mapped Value:");
+  Serial.println(mappedValue);
+  analogWrite(2,mappedValue);
+  //delay(100);
+  }
+  
+  }
+
+
+
